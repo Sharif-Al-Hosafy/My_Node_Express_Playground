8a63b705ff17ad50b85eab5289fe0be02cc59816
Sharif Al Hosafy <sherifismail44@gmail.com>
2022-06-04 22:10:15 +0200

    Dynamic Numeric Filters

--- 1- Store-Api/src/entities/products/product.controller.js
+++ 1- Store-Api/src/entities/products/product.controller.js
@@ -3,7 +3,8 @@ const Product = require("./product.model");
 // here the user can search and filter for products by many options
 // maybe by the name, company, price or ratings
 const getAllProducts = async (req, res) => {
-  const { featured, price, company, name, sort, fields } = req.query;
+  const { featured, price, company, name, sort, fields, numericFilters } =
+    req.query;
 
   // we made this empty obj in order not to mess with the query if a garbage params were sent
   const queryObj = {};
@@ -11,6 +12,28 @@ const getAllProducts = async (req, res) => {
   if (featured) queryObj.featured = featured === "true" ? true : false;
   if (company) queryObj.company = company;
   if (name) queryObj.name = { $regex: name, $options: "i" }; // search for any matchings with options case insensetive
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObj[field] = { [operator]: Number(value) };
+      }
+    });
+  }
 
   let result = Product.find(queryObj);
   if (sort) {
