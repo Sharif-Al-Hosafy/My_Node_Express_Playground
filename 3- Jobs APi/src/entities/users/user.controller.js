a90879ca99c2c6f1c8c8f9c0f24ab4edd908d73d
Sharif Al Hosafy <sherifismail44@gmail.com>
2022-06-15 05:14:48 +0200

    create token and login

--- 3- Jobs APi/src/entities/users/user.controller.js
+++ 3- Jobs APi/src/entities/users/user.controller.js
@@ -1,14 +1,31 @@
-const User = require('./user.model')
-const jwt = require('jsonwebtoken')
-
-const login = async (req, res) => {}
+const User = require("./user.model");
+const jwt = require("jsonwebtoken");
+const err = require("../../utils/createError");
 
 const register = async (req, res) => {
-  const newUser = await User.create({ ...req.body })
-  res.status(201).json({ status: 'success', user: newUser })
-}
+  const newUser = await User.create({ ...req.body });
+  const token = newUser.CreateJWT();
+  res.status(201).json({ user: { name: newUser.name }, token });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    throw err(400, "Please provide your name & your password");
+
+  const user = await User.findOne({ email });
+  if (!user) throw err(401, "user is not found");
+
+  const isPasswordMatch = await user.comparePass(password);
+  if (!isPasswordMatch) throw err(401, "Username or Password is incorrect");
+
+  const token = user.CreateJWT();
+
+  res.status(200).json({ token });
+};
 
 module.exports = {
   login,
   register,
-}
+};
--- 3- Jobs APi/src/entities/users/user.model.js
+++ 3- Jobs APi/src/entities/users/user.model.js
@@ -1,32 +1,46 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please provide name'],
+    required: [true, "Please provide name"],
     maxlength: 50,
     minlength: 4,
   },
   email: {
     type: String,
-    required: [true, 'Please provide email'],
+    required: [true, "Please provide email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please provide a valid email',
+      "Please provide a valid email",
     ],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
+    required: [true, "Please provide password"],
     minlength: 6,
   },
-})
+});
 
-UserSchema.pre('save', async function () {
-  const salt = await bcrypt.genSaltSync(10)
-  this.password = await bcrypt.hashSync(this.password, salt)
-})
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hashSync(this.password, salt);
+});
 
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.methods.CreateJWT = function () {
+  return jwt.sign(
+    { userId: this._id, username: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
+
+UserSchema.methods.comparePass = async function (userPass) {
+  const isMatch = await bcrypt.compare(userPass, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
